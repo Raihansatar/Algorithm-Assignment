a7a69da8625703b0560327336fa35b34d7f18cf8
It's ya boi <56668785+AjwadA@users.noreply.github.com>
2020-05-30 11:10:28 +0800

    Add positive and negative words textfile. ignore some of the files in the "Experimenting folder" as some of the files are made to clean up the Positive_Words.txt and Negative_Words.txt file content

---
+++ ExperimentingFolder/cleaning WordText.py
@@ -0,0 +1,24 @@
+# with open (".txt", encoding="utf8") as myfile:
+#     data=myfile.read()
+#     wordlist = data.split(",")
+#     print(wordlist);
+#     for i in range(len(wordlist)):
+#         while(wordlist[i][0].startswith(" ")):
+#             print(str(wordlist[i]))
+#             wordlist[i] = wordlist[i][1:]
+#     print(wordlist)
+#
+# myfile.close()
+#
+# with open("../textfile/.txt","a+") as f:
+#     for word in wordlist:
+#         f.write(str(word)+",");
+#
+# f.close()
+# print(wordlist)
+#
+# with open("../textfile/.txt","r") as f:
+#     data=f.read();
+#     myList = data.split(",");
+#
+#     print(myList)
